11b12ca2dfb5f8b7363293a93f15ee1080acc33b
TuanAnh <dangtuananh3012003@gmail.com>
2023-10-02 20:44:53 +0700

    updateSecond

--- sources/main.js
+++ sources/main.js
@@ -3,3 +3,16 @@ document.body.innerHTML = 'chao ban';
 // console.log(document.getElementById('dang_tuan_anh'));
 console.log(document.getElementsByClassName('html .box1'));
 document.getAnimations("sfsdf");
+document.getAnimations("sfsdf");
+
+document.getAnimations("sfsdf");
+
+document.getAnimations("sfsdf");
+
+document.getAnimations("sfsdf");
+
+console.log(document.getElementsByClassName('html .box1'));
+console.log(document.getElementsByClassName('html .box1'));
+console.log(document.getElementsByClassName('html .box1'));
+console.log(document.getElementsByClassName('html .box1'));
+console.log(document.getElementsByClassName('html .box1'));
