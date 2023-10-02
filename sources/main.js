d615667e15e47a7b4bacb6ad27015b9a5256468b
TuanAnh <dangtuananh3012003@gmail.com>
2023-10-02 20:34:43 +0700

    update

--- sources/main.js
+++ sources/main.js
@@ -2,3 +2,4 @@ alert('Đây là trang chủ');
 document.body.innerHTML = 'chao ban';
 // console.log(document.getElementById('dang_tuan_anh'));
 console.log(document.getElementsByClassName('html .box1'));
+document.getAnimations("sfsdf");
